1b4920806e3d163d7d62acae0e6fc2f897b9b26b
Yaraslau Kurmyza <me@yaraslav.com>
2015-12-29 17:06:00 +0100

    Trying to fetch via api GTM

--- README.md
+++ README.md
@@ -9,6 +9,26 @@ Save it in the containers folder or anywhere else.
 ## Run the tool
 
 
-        node index.js path/to/GTM-XXYYZZ-vJJ.json
+        node gtm-validator.js local path/to/GTM-XXYYZZ-vJJ.json
 
-It will output debug information 
+        node gtm-validator.js gtm:list <accountId>
+
+        node gtm-validator.js gtm:download <accountId> <containerId>
+
+
+It will output debug information
+
+
+
+## API
+https://developers.google.com/tag-manager/api/v1/reference/accounts/containers/tags/update
+https://github.com/google/google-api-nodejs-client/
+
+
+### Get keys: https://developers.google.com/identity/protocols/application-default-credentials
+Generate OAuth keys from https://console.developers.google.com/home/dashboard
+Export them:
+
+    export GAPI_CLIENT_ID='your-client-id'
+    export GAPI_CLIENT_SECRET='your-client-secret'
+
---
+++ gtm-validator.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var program = require('commander');
+
+var checkLocal = require('./lib/check-local');
+var gtm = require('./lib/gtm');
+
+var cmd;
+
+program.version('0.0.1')
+    .command('gtm:download <accountId>')
+    .description('Download containers using GAPI_KEY and storing in containers/ folder')
+    .action((accountId, opts) => {
+        cmd = 'gtm:download';
+        gtm.download(accountId, opts);
+    });
+
+program
+    .command('gtm:list <accountId>')
+    .description('List GTM accounts')
+    .action((accountId, opts) => {
+        cmd = 'gtm:list';
+        gtm.list(accountId, opts);
+    });
+
+program
+    .command('local <files>')
+    .description('Validate local json file')
+    .action((files) => {
+        cmd = 'local';
+        checkLocal(files);
+    });
+
+program.parse(process.argv);
+
+if (!cmd) {
+    program.outputHelp();
+}
